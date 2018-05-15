8b0d6dcf40d1011da59efd30b27e3605b2b38b0d
shalun002 <32149071+shalun002@users.noreply.github.com>
2018-05-15 13:10:35 +0600

    Add project files.

---
+++ Properties/Module/Cat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties.Module
+{
+    public enum Food { mouse, fish, whiskass, meat, ibuprofen }
+
+    public struct Cat
+    {
+        public int SatietyLevel { get; set; }
+
+        public void EatSomething()
+        {
+            Food food = new Food();
+            Random rand = new Random();
+            food = (Food)rand.Next(0, 4);
+            switch (food)
+            {
+                case Food.fish:
+                    SatietyLevel = 50;
+                    Console.WriteLine($"The Cat was fed {food} and satiet level is {SatietyLevel}");
+                    break;
+                case Food.meat:
+                    SatietyLevel = 49;
+                    Console.WriteLine($"The Cat was fed {food} and satiet level is {SatietyLevel}");
+                    break;
+                case Food.mouse:
+                    SatietyLevel = 30;
+                    Console.WriteLine($"The Cat was fed {food} and satiet level is {SatietyLevel}");
+                    break;
+                case Food.whiskass:
+                    SatietyLevel = 20;
+                    Console.WriteLine($"The Cat was fed {food} and satiet level is {SatietyLevel}");
+                    break;
+                case Food.ibuprofen:
+                    SatietyLevel = 0;
+                    Console.WriteLine($"The Cat was fed {food} and cat is died out");
+                    break;
+            }
+        }
+    }
+}
